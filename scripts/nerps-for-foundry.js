6b9bcf1e956552d9db4735b2fad7822dd4f2eaa6
Jamz <JamzTheMan@Gmail.com>
2025-10-05 11:53:50 -0500

    Refactor RemoveReactionEffects method for improved error handling and readability

--- scripts/nerps-for-foundry.js
+++ scripts/nerps-for-foundry.js
@@ -5,29 +5,33 @@ import {Logger} from "./utils/logger.js";
 export let log = new Logger();
 
 export let i18n = key => {
-  return game.i18n.localize(key);
+    return game.i18n.localize(key);
 };
 
 export class NerpsForFoundry {
-  async RemoveReactionEffects(combatantActorId, expiryText) {
-    // console.log(currentCombatant.actor.items);
-    let combatantActor = game.combat.getCombatantByActor(combatantActorId)?.actor;
-    let reactionEffectIds = combatantActor.items
-        .filter(item => item.type === 'effect')
-        .filter(item => item.name.startsWith('Reaction: '))
-        .filter(item => item.system.duration.expiry === expiryText)
-        .map(item => item._id);
+    async RemoveReactionEffects(combatantActorId, expiryText) {
+        let combatant = game.combat.getCombatantByActor(combatantActorId);
+        if (!combatant || !combatant.actor) {
+            log.info(`RemoveReactionEffects: No combatant or actor found for actorId: ${combatantActorId}`);
+            return;
+        }
+        let combatantActor = combatant.actor;
+        let reactionEffectIds = combatantActor.items
+            .filter(item => item.type === 'effect')
+            .filter(item => item.name.startsWith('Reaction: '))
+            .filter(item => item.system.duration.expiry === expiryText)
+            .map(item => item._id);
 
-    await combatantActor.deleteEmbeddedDocuments("Item", reactionEffectIds);
-  }
+        await combatantActor.deleteEmbeddedDocuments("Item", reactionEffectIds);
+    }
 
-  loadCustomCssOverrides() {
-    const head = document.getElementsByTagName("head")[0];
-    const mainCss = document.createElement("link");
-    mainCss.setAttribute("rel", "stylesheet")
-    mainCss.setAttribute("type", "text/css")
-    mainCss.setAttribute("href", `${MODULE_PATH}/styles/custom-css-overrides.css`)
-    mainCss.setAttribute("media", "all")
-    head.insertBefore(mainCss, head.lastChild);
-  }
+    loadCustomCssOverrides() {
+        const head = document.getElementsByTagName("head")[0];
+        const mainCss = document.createElement("link");
+        mainCss.setAttribute("rel", "stylesheet")
+        mainCss.setAttribute("type", "text/css")
+        mainCss.setAttribute("href", `${MODULE_PATH}/styles/custom-css-overrides.css`)
+        mainCss.setAttribute("media", "all")
+        head.insertBefore(mainCss, head.lastChild);
+    }
 }
