f573df6e2beee88ca0dcdf82a35e9def5cdf3198
Jamz <JamzTheMan@Gmail.com>
2026-02-02 14:45:25 -0600

    refactor: Remove unused journal editor tools and related settings

--- scripts/hooks-for-nerps.js
+++ scripts/hooks-for-nerps.js
@@ -1,5 +1,4 @@
-import {autoCorrectJournalContent} from "./autocorrect-journal-content.js"
-import {JOURNAL_MARKER, MODULE_NAME} from "./constants.js";
+import {MODULE_NAME} from "./constants.js";
 import {adjustShieldHP} from "./macros/adjust-shield-hp.js";
 import {
     checkForPf2ePerceptionConditions,
@@ -47,64 +46,6 @@ v${game.modules.get(MODULE_NAME).version}
 
     registerSettings();
 
-    if (getSetting('journal-editor-tools')) {
-        tinymce.PluginManager.add("nerpsJournalFix", function (editor) {
-            editor.ui.registry.addButton("nerpsJournalFix", {
-                tooltip: "Apply autocorrections to journal, infused with Nerps.",
-                icon: "format-painter",
-                onAction: function () {
-                    editor.setDirty(true)
-                    const selectedContent = editor.selection.getContent();
-                    if (selectedContent === "") {
-                        const newJournalContent = autoCorrectJournalContent(editor.getContent());
-                        editor.setContent(newJournalContent);
-                    } else {
-                        const newSelectedContent = autoCorrectJournalContent(selectedContent);
-                        editor.selection.setContent(newSelectedContent);
-                    }
-
-                    const newContent = editor.getContent().replaceAll(JOURNAL_MARKER, "");
-                    editor.setContent(`${newContent}`);
-                    // editor.setContent(`${newContent}\n${JOURNAL_MARKER}`);
-                },
-            });
-        });
-
-        tinymce.PluginManager.add("nerpsJournalPasteAndFix", function (editor) {
-            editor.ui.registry.addButton("nerpsJournalPasteAndFix", {
-                tooltip: "Paste from clipboard with autocorrections, infused with Nerps, to the journal.",
-                icon: "paste",
-                onAction: function () {
-                    editor.setDirty(true)
-                    navigator.clipboard.readText().then(pasteContent => {
-                        editor.insertContent(autoCorrectJournalContent(pasteContent));
-                        const newContent = editor.getContent().replaceAll(JOURNAL_MARKER, "");
-                        editor.setContent(`${newContent}`);
-                        // editor.setContent(`${newContent}\n${JOURNAL_MARKER}`);
-                    })
-                },
-            });
-        });
-
-        // Convenience button to set text to block quote and font size 14
-        tinymce.PluginManager.add("nerpsJournalBlockquote", function (editor) {
-            editor.ui.registry.addButton("nerpsJournalBlockquote", {
-                tooltip: "Format selected text, infused with Nerps, to the journal.",
-                icon: "quote",
-                onAction: function () {
-                    const selectedContent = editor.selection.getContent();
-                    if (selectedContent !== "") {
-                        editor.setDirty(true)
-                        editor.execCommand("mceBlockQuote");
-                        editor.execCommand('FontSize', false, '14pt');
-                    }
-                },
-            });
-        });
-
-        CONFIG.TinyMCE.plugins = CONFIG.TinyMCE.plugins + " nerpsJournalFix nerpsJournalPasteAndFix nerpsJournalBlockquote";
-        CONFIG.TinyMCE.toolbar = CONFIG.TinyMCE.toolbar + " nerpsJournalFix nerpsJournalPasteAndFix nerpsJournalBlockquote";
-    }
 
     /*
       Export out functions to be used in macro scripts...
--- scripts/settings-for-nerps.js
+++ scripts/settings-for-nerps.js
@@ -2,10 +2,6 @@ import {MODULE_NAME} from "./constants.js";
 
 // Register any custom module settings here
 export const registerSettings = function () {
-    const debouncedReload = foundry.utils.debounce(function () {
-        window.location.reload();
-    }, 100);
-
     game.settings.register(MODULE_NAME, "auto-remove-reaction-effects", {
         name: "Auto-Remove Reaction Effects",
         hint: "Automatically remove effects that start with the name 'Reaction: ' at the start (or end based on duration settings) of the tokens turn in combat.",
@@ -51,33 +47,6 @@ export const registerSettings = function () {
         config: true
     });
 
-    game.settings.register(MODULE_NAME, "journal-editor-tools", {
-        name: "Add Autocorrect toolbar buttons to Journal Editor",
-        hint: "Tools to attempt to correct text pasted from PDF.",
-        type: Boolean,
-        default: false,
-        scope: "world",
-        config: true,
-        onChange: debouncedReload
-    });
-
-    game.settings.register(MODULE_NAME, "additional-auto-correct-rules", {
-        name: "Autocorrect RegEx Rules",
-        hint: "Add these regular expressions to auto-correct default rules.",
-        type: String,
-        default: `[]`,
-        scope: "world",
-        config: true
-    });
-
-    game.settings.register(MODULE_NAME, "auto-correct-f-words", {
-        name: "Autocorrect F words",
-        hint: "[EXPERIMENTAL] Copy from PDFoundry oddly adds a space after the 'f' in many words.",
-        type: String,
-        default: `Jorgenf ist, inf luence, aff luent, campf ire`,
-        scope: "world",
-        config: true
-    });
 
     game.settings.register(MODULE_NAME, "debug-mode", {
         name: "Toggle Debug Mode",
